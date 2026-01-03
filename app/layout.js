fe80993a0a17a9db63fc434d80c2b94b2a87e223
Ansh Shukla <anshshuklavg@gmail.com>
2026-01-03 10:54:06 +0530

    Update dependencies and deploy changes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import bitlink from "../public/bitlink.png";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
